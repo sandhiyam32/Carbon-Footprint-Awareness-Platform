8a9ab7df02b80a24bdc9abfc6801ec7bb2d4fa24
Sandhiya M <msandhiya4242@gmail.com>
2026-06-19 21:55:02 +0530

    url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || '/api';
+const API_URL = "https://carbon-footprint-awareness-platform.onrender.com/api";
 
 const api = axios.create({ baseURL: API_BASE_URL });
 
